6254caf806e0ad08d5f312cdd8f06efb4d0b6d42
Иван Фоменко <fomencatbig122@gmail.com>
2025-09-27 23:06:50 +0300

    das

---
+++ TodoList/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Todolist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Работу выполнили Фоменко и Мортиросян");
+
+            Console.Write("Введите ваше имя: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Введите вашу фамилию: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Введите ваш год рождения: ");
+            string yearBirthString = Console.ReadLine();
+
+            int yearBirth = int.Parse(yearBirthString);
+
+            int age = DateTime.Now.Year - yearBirth;
+
+            Console.WriteLine($"Добавлен пользователь {firstName} {lastName}, возраст – {age}");
+
+        }
+    }
+}
