cdb608fa22502c0017aca720679275f921fe30fc
Иван Фоменко <fomencatbig122@gmail.com>
2025-10-03 18:59:14 +0300

    Added an infinite loop and help, exit commands

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace Todolist
+﻿using System;
+
+namespace Todolist
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Работу выполнили Фоменко и Мортиросян");
+            Console.WriteLine("Работу выполнили Фоменко и Мартиросьян");
 
             Console.Write("Введите ваше имя: ");
             string firstName = Console.ReadLine();
@@ -16,11 +18,44 @@
             string yearBirthString = Console.ReadLine();
 
             int yearBirth = int.Parse(yearBirthString);
-
             int age = DateTime.Now.Year - yearBirth;
 
             Console.WriteLine($"Добавлен пользователь {firstName} {lastName}, возраст – {age}");
+            Console.WriteLine("Добро пожаловать в систему управления задачами!");
+            Console.WriteLine("Введите 'help' для списка команд");
 
+            // Основной цикл программы
+            while (true)
+            {
+                Console.Write("> ");
+                string input = Console.ReadLine();
+                
+                if (string.IsNullOrWhiteSpace(input))
+                    continue;
+                    
+                string[] parts = input.Split(' ');
+                string command = parts[0].ToLower();
+                
+                switch (command)
+                {
+                    case "help":
+                        ShowHelp();
+                        break;
+                    case "exit":
+                        Console.WriteLine("Выход из программы...");
+                        return;
+                    default:
+                        Console.WriteLine($"Неизвестная команда: {command}");
+                        break;
+                }
+            }
+        }
+
+        static void ShowHelp()
+        {
+            Console.WriteLine("Доступные команды:");
+            Console.WriteLine("help    - вывести список команд");
+            Console.WriteLine("exit    - выход из программы");
         }
     }
 }
