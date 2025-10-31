7ffe1920f4d1025fa538a3e25ebcf7cc9b4fed66
fomenkoIvanKLL <gagkgvoaoaa@gmail.com>
2025-10-31 23:01:44 +0300

    lesson 7: add HelpCommand.cs

---
+++ TodoList/HelpCommand.cs
@@ -0,0 +1,26 @@
+namespace TodoList;
+
+public class HelpCommand: ICommand
+{
+    public void Execute()
+    {
+        Console.WriteLine("Доступные команды:");
+        Console.WriteLine("help                          - показать эту справку");
+        Console.WriteLine("add [--multiline|-m] <task>   - добавить задачу (многострочный режим с флагом)");
+        Console.WriteLine("view [--index|-i] [--status|-s] [--update-date|-d] [--all|-a]");
+        Console.WriteLine("                              - показать задачи с флагами отображения");
+        Console.WriteLine("read <idx>                    - прочитать полный текст задачи");
+        Console.WriteLine("done <num>                    - отметить задачу как выполненную");
+        Console.WriteLine("delete <num>                  - удалить задачу");
+        Console.WriteLine("update <num> \"<text>\"       - обновить текст задачи");
+        Console.WriteLine("profile                       - управление профилем пользователя");
+        Console.WriteLine("exit                          - выйти из программы");
+        Console.WriteLine();
+        Console.WriteLine("Флаги для команды view:");
+        Console.WriteLine("  --index, -i       - показывать индекс задачи");
+        Console.WriteLine("  --status, -s      - показывать статус задачи");
+        Console.WriteLine("  --update-date, -d - показывать дату изменения");
+        Console.WriteLine("  --all, -a         - показывать все данные");
+        Console.WriteLine("Комбинации флагов: view -is, view --index --status и т.д.");
+    }
+}
