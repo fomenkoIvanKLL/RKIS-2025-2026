ee17cc725be0b19a8e9f921a83a79b6d601c5753
fomenkoIvanKLL <gagkgvoaoaa@gmail.com>
2025-11-15 15:42:15 +0300

    lesson 9: fix view

--- TodoList/TodoList.cs
+++ TodoList/TodoList.cs
@@ -23,8 +23,8 @@ public class TodoList : IEnumerable<TodoItem>
 
 		var header = "";
 		if (showIndex) header += "№".PadRight(6);
-		if (showStatus) header += "Статус".PadRight(10);
-		if (showDate) header += "Дата".PadRight(16);
+		if (showStatus) header += "Статус".PadRight(16);
+		if (showDate) header += "Дата".PadRight(20);
 		header += "Задача";
 
 		Console.WriteLine(header);
@@ -34,8 +34,8 @@ public class TodoList : IEnumerable<TodoItem>
 		{
 			var line = "";
 			if (showIndex) line += $"{i + 1}".PadRight(6);
-			if (showStatus) line += $"{items[i].Status}".PadRight(10);
-			if (showDate) line += $"{items[i].LastUpdate:dd.MM.yyyy HH:mm}".PadRight(16);
+			if (showStatus) line += $"{items[i].Status}".PadRight(16);
+			if (showDate) line += $"{items[i].LastUpdate:dd.MM.yyyy HH:mm}".PadRight(20);
 
 			var preview = items[i].Text.Length <= 30 ? items[i].Text : items[i].Text.Substring(0, 27) + "...";
 			line += preview;
