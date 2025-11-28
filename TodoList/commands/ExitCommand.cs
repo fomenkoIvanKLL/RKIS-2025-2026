3a9783ac73074058bcb6aaf7014fe00a57c071eb
Иван Фоменко <fomencatbig122@gmail.com>
2025-11-28 23:55:16 +0300

    Lesson 10: realized metod Unexecute for ExitCommand

--- TodoList/commands/ExitCommand.cs
+++ TodoList/commands/ExitCommand.cs
@@ -2,9 +2,14 @@ namespace TodoList.commands;
 
 public class ExitCommand : ICommand
 {
-	public void Execute()
-	{
-		Console.WriteLine("Выход из программы. До свидания!");
-		Environment.Exit(0);
-	}
+    public void Execute()
+    {
+        Console.WriteLine("Выход из программы. До свидания!");
+        Environment.Exit(0);
+    }
+
+    public void Unexecute()
+    {
+        // Команда exit не требует отмены
+    }
 }
