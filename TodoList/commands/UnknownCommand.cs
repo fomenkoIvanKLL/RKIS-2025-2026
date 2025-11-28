ea3541bc7dd55aebf64bec1d49e1d8eb9291e165
Иван Фоменко <fomencatbig122@gmail.com>
2025-11-28 23:55:54 +0300

    Lesson 10: realized metod Unexecute for UnknownCommand

--- TodoList/commands/UnknownCommand.cs
+++ TodoList/commands/UnknownCommand.cs
@@ -2,8 +2,13 @@ namespace TodoList.commands;
 
 public class UnknownCommand : ICommand
 {
-	public void Execute()
-	{
-		Console.WriteLine("Неизвестная команда");
-	}
+    public void Execute()
+    {
+        Console.WriteLine("Неизвестная команда");
+    }
+
+    public void Unexecute()
+    {
+        // Команда unknown не требует отмены
+    }
 }
