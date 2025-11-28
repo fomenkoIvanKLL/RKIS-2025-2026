dd901f9238a1bdbcb0f5261fd6e665b3c53d2e58
Иван Фоменко <fomencatbig122@gmail.com>
2025-11-28 23:52:22 +0300

    Lesson 10: realized metod Unexecute for SetStatusCommand

--- TodoList/commands/SetStatusCommand.cs
+++ TodoList/commands/SetStatusCommand.cs
@@ -2,28 +2,42 @@ namespace TodoList.commands;
 
 public class SetStatusCommand : ICommand
 {
-	public string[] parts { get; set; }
-	public TodoList todoList { get; set; }
+    public string[] parts { get; set; }
+    public TodoItem StatusItem { get; private set; }
+    public TodoStatus OldStatus { get; private set; }
+    public TodoStatus NewStatus { get; private set; }
 
-	public void Execute()
-	{
-		if (parts.Length < 2 || !int.TryParse(parts[1], out var taskNumber))
-		{
-			Console.WriteLine("Ошибка: укажите номер задачи");
-			return;
-		}
+    public void Execute()
+    {
+        if (parts.Length < 2 || !int.TryParse(parts[1], out var taskNumber))
+        {
+            Console.WriteLine("Ошибка: укажите номер задачи");
+            return;
+        }
 
-		var index = taskNumber - 1;
-		try
-		{
-			var status = Enum.Parse<TodoStatus>(parts[2], true);
-			var item = todoList.GetItem(index);
-			item.SetStatus(status);
-			Console.WriteLine($"Поставлен новый статус({status}) для задачи '{item.Text}' ");
-		}
-		catch (Exception ex)
-		{
-			Console.WriteLine($"Ошибка: {ex.Message}");
-		}
-	}
+        var index = taskNumber - 1;
+        try
+        {
+            StatusItem = AppInfo.Todos.GetItem(index);
+            OldStatus = StatusItem.Status;
+            NewStatus = Enum.Parse<TodoStatus>(parts[2], true);
+            
+            StatusItem.SetStatus(NewStatus);
+            Console.WriteLine($"Поставлен новый статус({NewStatus}) для задачи '{StatusItem.Text}'");
+            AppInfo.UndoStack.Push(this);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+    }
+
+    public void Unexecute()
+    {
+        if (StatusItem != null)
+        {
+            StatusItem.SetStatus(OldStatus);
+            Console.WriteLine($"Отменена смена статуса. Восстановлен статус: {OldStatus}");
+        }
+    }
 }
