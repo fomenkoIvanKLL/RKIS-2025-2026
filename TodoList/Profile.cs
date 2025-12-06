c0895eca1910e488900228bf39ce43426fb26f4c
Иван Фоменко <fomencatbig122@gmail.com>
2025-12-06 21:58:08 +0300

    Lesson 11: update class Profile

--- TodoList/Profile.cs
+++ TodoList/Profile.cs
@@ -2,20 +2,41 @@ namespace TodoList;
 
 public class Profile
 {
-	public Profile(string firstName, string lastName, int birthYear)
-	{
-		FirstName = firstName;
-		LastName = lastName;
-		BirthYear = birthYear;
-	}
+    public Profile(string login, string password, string firstName, string lastName, int birthYear)
+    {
+        Id = Guid.NewGuid();
+        Login = login;
+        Password = password;
+        FirstName = firstName;
+        LastName = lastName;
+        BirthYear = birthYear;
+    }
 
-	public string FirstName { get; private set; }
-	public string LastName { get; private set; }
-	public int BirthYear { get; private set; }
+    public Profile(Guid id, string login, string password, string firstName, string lastName, int birthYear)
+    {
+        Id = id;
+        Login = login;
+        Password = password;
+        FirstName = firstName;
+        LastName = lastName;
+        BirthYear = birthYear;
+    }
 
-	public string GetInfo()
-	{
-		var age = DateTime.Now.Year - BirthYear;
-		return $"{FirstName} {LastName}, возраст {age}";
-	}
+    public Guid Id { get; }
+    public string Login { get; }
+    public string Password { get; }
+    public string FirstName { get; }
+    public string LastName { get; }
+    public int BirthYear { get; }
+
+    public string GetInfo()
+    {
+        var age = DateTime.Now.Year - BirthYear;
+        return $"{FirstName} {LastName}, возраст {age}";
+    }
+    
+    public bool CheckPassword(string password)
+    {
+        return Password == password;
+    }
 }
