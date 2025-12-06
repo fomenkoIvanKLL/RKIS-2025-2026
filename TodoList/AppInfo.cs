17f359fd346ab6c418774fc00be81a841c48863d
Иван Фоменко <fomencatbig122@gmail.com>
2025-12-06 21:59:28 +0300

    Lesson 11: add multiuser AppInfo

--- TodoList/AppInfo.cs
+++ TodoList/AppInfo.cs
@@ -5,8 +5,22 @@ namespace TodoList;
 
 public static class AppInfo
 {
-    public static TodoList Todos { get; set; } = new TodoList();
-    public static Profile CurrentProfile { get; set; } = new Profile("Default", "User", 2000);
+    public static Dictionary<Guid, TodoList> TodosByUser { get; set; } = new Dictionary<Guid, TodoList>();
+    public static List<Profile> Profiles { get; set; } = new List<Profile>();
+    public static Guid? CurrentProfileId { get; set; }
+    public static Profile CurrentProfile => 
+        CurrentProfileId.HasValue ? Profiles.FirstOrDefault(p => p.Id == CurrentProfileId) : null;
     public static Stack<ICommand> UndoStack { get; set; } = new Stack<ICommand>();
     public static Stack<ICommand> RedoStack { get; set; } = new Stack<ICommand>();
+    
+    public static TodoList GetCurrentTodoList()
+    {
+        if (!CurrentProfileId.HasValue)
+            throw new InvalidOperationException("Нет активного профиля");
+        
+        if (!TodosByUser.ContainsKey(CurrentProfileId.Value))
+            TodosByUser[CurrentProfileId.Value] = new TodoList();
+        
+        return TodosByUser[CurrentProfileId.Value];
+    }
 }
