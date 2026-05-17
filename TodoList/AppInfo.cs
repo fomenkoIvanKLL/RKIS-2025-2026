e162b5bb470c4993723f7aaefaaae776010642e1
ivan <gagkgvoaoaa@gmail.com>
2026-05-17 21:01:55 +0300

    lesson 7, semestr 2 (RKIS): Create Repositories and Update AppInfo.cs

--- TodoList/AppInfo.cs
+++ TodoList/AppInfo.cs
@@ -1,29 +1,32 @@
-using System.Collections.Generic;
+using TodoList.Services;
 using TodoList.commands;
-using TodoList.Exceptions;
 
 namespace TodoList;
 
 public static class AppInfo
 {
+    public static TodoRepository TodoRepo { get; set; } = new TodoRepository();
+    public static ProfileRepository ProfileRepo { get; set; } = new ProfileRepository();
+
+    // Для обратной совместимости (некоторые команды могут ещё использовать TodosByUser)
     public static Dictionary<Guid, TodoList> TodosByUser { get; set; } = new Dictionary<Guid, TodoList>();
     public static List<Profile> Profiles { get; set; } = new List<Profile>();
+
     public static Guid? CurrentProfileId { get; set; }
-    public static Profile CurrentProfile => 
-        CurrentProfileId.HasValue ? Profiles.FirstOrDefault(p => p.Id == CurrentProfileId) : null;
+    public static Profile CurrentProfile => CurrentProfileId.HasValue ? ProfileRepo.GetById(CurrentProfileId.Value) : null;
+
     public static Stack<ICommand> UndoStack { get; set; } = new Stack<ICommand>();
     public static Stack<ICommand> RedoStack { get; set; } = new Stack<ICommand>();
-    
-    public static IDataStorage? DataStorage { get; set; }
 
+    // Метод для получения текущего списка задач (кэш, но можно переделать)
     public static TodoList GetCurrentTodoList()
     {
         if (!CurrentProfileId.HasValue)
-            throw new ProfileNotFoundException("Нет активного профиля. Сначала войдите или создайте профиль.");
-        
+            throw new Exceptions.ProfileNotFoundException("Нет активного профиля.");
+
         if (!TodosByUser.ContainsKey(CurrentProfileId.Value))
             TodosByUser[CurrentProfileId.Value] = new TodoList();
-        
+
         return TodosByUser[CurrentProfileId.Value];
     }
 }
